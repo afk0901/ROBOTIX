cbfe61aa87bccf4eae9b134d48171f1bf8d38a92
Arnar Freyr Kristinsson <arnarfreyr@hive.is>
2016-12-05 13:14:42 +0000

    kodi

--- kodi.ino
+++ kodi.ino
@@ -1,52 +1,32 @@
-
-
 int i;
 int expression = 0;
-const int maxselect = 3;//How many mixes.
-int blondur1[3] = {8,9,10};
+int blondur1[3] = {11,9,10};
 int blondur2[3] = {8,10,11};
 int blondur3[4] = {8,9,10,11};
-int blondur4[5] = {7,8,9,10,11};
-
-int runloop = true;
+int blondur4[5] = {4,8,9,10,11};
+int blondur5[4] = {4,9,11,10};
 int countbutton = 0;
 int count1 = 0;
-
-// constants won't change. They're used here to
-// set pin numbers:
-const int buttonPin = 3;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int buttonPin = 3;    
 const int buttonPin_select = 2;
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int buttonState = 0;         
 int buttonStateselect = 0;
 int start = 0;
 int resetPin = 12;
 
-
-
-
+//uses a for loop to loop through the selenoids. It's equal liquid flowing through each one. 
 void mix(int l,int arr[])
 {
  
   for (i=0; i < l; i = i +1)
   {
     delay(1000);
-    digitalWrite(arr[i], HIGH);   // opnar fyrir solinoid
-    delay(1000);              // í 1 sek
-    digitalWrite(arr[i], LOW);    //lokar solinoid
-    delay(1000);              // í eina sek
+    digitalWrite(arr[i], HIGH);   
+    delay(1000);              
+    digitalWrite(arr[i], LOW);    
+    start++;              
   }
 }
-
-void mixingmsg(String msg)
-{
-  while(runloop)
-  {
-    runloop = false;
-  }
-}
-
 
 void setup()
 {
@@ -57,38 +37,17 @@ void setup()
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   
-
-  // set up the LCD's number of columns and rows:
-  // Print a message to the LCD.
-  // initialize the LED pin as an output:
-
   Serial.begin(9600);
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
   pinMode(buttonPin_select, INPUT);
 
-
-}
-
-
-//int buttonState = 0;         // variable for reading the pushbutton status
-//int buttonStateselect = 0;
-
-
-void loop()
-{
-
-  //mixingmsg("Pick a mix");
-
-  
-  
-
+Serial.print("PICK A MIX!");
+//We use a while loop to wait for the user to press the button. Teh button state is LOW when it's pressed.
 while(start == 0)
 {
   buttonState = digitalRead(buttonPin);
   buttonStateselect = digitalRead(buttonPin_select);
+  
   if (buttonState == LOW)
   {
     
@@ -112,43 +71,55 @@ while(start == 0)
   
   if (buttonStateselect == LOW)
    {
-    start++;
+    
     expression = count1;
     Serial.print("Selected"); 
+    break;
    }
  
 }
-
   switch(expression)
   {
   
     case 1:
-    Serial.print(count1);
-    mix(4,  blondur3);
+     Serial.print("Mixing mix number 1...");
+    mix(3,blondur1);
     pinMode(resetPin, OUTPUT);
+    Serial.print("DONE");
     break;
     
     case 2:
-    Serial.print("Case 2");
+    Serial.print("Mixing mix number 2...");
     mix(3, blondur1);
     pinMode(resetPin, OUTPUT);
+     Serial.print("DONE");
     break;
     
     case 3:
-    Serial.print(count1);
+    Serial.print("Mixing mix number 3...");
     mix(3, blondur2);
     pinMode(resetPin, OUTPUT);
+    Serial.print("DONE");
     break;
     
     case 4:
+     Serial.print("Mixing mix number 4...");
     mix(5, blondur4);
     pinMode(resetPin, OUTPUT);
     break;
     
     case 5:
+     Serial.print("Mixing mix number 5...");
+    mix(4, blondur5);
     pinMode(resetPin, OUTPUT);
+    Serial.print("DONE");
     break;
   }
-}
   
+}
+
+
+void loop()
+{
+}
 
